c469c81c85442dd87394177a2b1e24f2a294b366
zrwusa <zrwusa@gmail.com>
2021-05-14 17:00:46 +0800

    when run yarn android

---
+++ android/app/src/main/java/com/zrwusa/exporeactbunny/generated/BasePackageList.java
@@ -0,0 +1,35 @@
+package com.zrwusa.exporeactbunny.generated;
+
+import java.util.Arrays;
+import java.util.List;
+import org.unimodules.core.interfaces.Package;
+
+public class BasePackageList {
+  public List<Package> getPackageList() {
+    return Arrays.<Package>asList(
+        new expo.modules.appauth.AppAuthPackage(),
+        new expo.modules.application.ApplicationPackage(),
+        new expo.modules.av.AVPackage(),
+        new expo.modules.camera.CameraPackage(),
+        new expo.modules.constants.ConstantsPackage(),
+        new expo.modules.device.DevicePackage(),
+        new expo.modules.errorrecovery.ErrorRecoveryPackage(),
+        new expo.modules.facebook.FacebookPackage(),
+        new expo.modules.filesystem.FileSystemPackage(),
+        new expo.modules.firebase.core.FirebaseCorePackage(),
+        new expo.modules.font.FontLoaderPackage(),
+        new expo.modules.haptics.HapticsPackage(),
+        new expo.modules.imageloader.ImageLoaderPackage(),
+        new expo.modules.imagepicker.ImagePickerPackage(),
+        new expo.modules.keepawake.KeepAwakePackage(),
+        new expo.modules.lineargradient.LinearGradientPackage(),
+        new expo.modules.localization.LocalizationPackage(),
+        new expo.modules.location.LocationPackage(),
+        new expo.modules.notifications.NotificationsPackage(),
+        new expo.modules.permissions.PermissionsPackage(),
+        new expo.modules.sharing.SharingPackage(),
+        new expo.modules.splashscreen.SplashScreenPackage(),
+        new expo.modules.updates.UpdatesPackage()
+    );
+  }
+}
